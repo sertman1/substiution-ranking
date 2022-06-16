48361748e9507a6772844569d597bf022a40b5bf
sertman1 <sertman1@jhu.edu>
2022-06-16 12:43:03 +0200

    made program compatibile for parallel text's format

--- textprocessor.cpp
+++ textprocessor.cpp
@@ -1,9 +1,4 @@
-// This program takes a text file (written in the English alphabet)
-// and outputs the frequency of each 26 letters in the text as well
-// as hyphens, if they are used to form a word (e.g., one-year-old).
-// Additionally, it outputs the frequency of the words' lengths,
-// as well as the average word length. 
-// This program assumes that the words in the text are spelled correctly. 
+// TODO:: program description
 
 // To be added: mapping characters' occurences to word length 
 // e.g., does 'x'/'X' appear more frequently in longer words?
@@ -17,22 +12,27 @@
 #include <map>
 using std::map;
 
-void print_data(map<char, unsigned> char_frequencies, map<unsigned, unsigned> length_frequencies);
+void print_data(map<char, unsigned> char_frequencies_complex, map<unsigned, unsigned> length_frequencies_complex);
 
 // N.B.: When executing the program, give it text through cin
 // e.g., './a.out < mytext.txt'
 int main() {
 	
 	std::string input_buffer;
-	map<char, unsigned> char_frequencies;
-	map<unsigned, unsigned> length_frequencies;
-	
+	bool simple_sentence = false;
+	map<char, unsigned> char_frequencies_complex;
+	map<unsigned, unsigned> length_frequencies_complex;
+	map<char, unsigned> char_frequencies_simple;
+	map<unsigned, unsigned> length_frequencies_simple;
+
 	// TODO::
-	//map<char, map<unsigned, unsigned>> char_to_length_frequencies;
+	//map<char, map<unsigned, unsigned>> char_to_length_frequencies_complex;
 	//map<char, map<unsigned, unsigned>> char_to_numberof_frequencies;
+	
 
 	while(std::getline(std::cin, input_buffer)) { // read each line of input text into buffer
-		for(unsigned i = 0; i < input_buffer.length(); i++) {
+		
+		for(unsigned i = 0; i < input_buffer.length(); i++ ) {
 			
 			char c = input_buffer.at(i);
 			unsigned word_length = 0;
@@ -40,7 +40,12 @@ int main() {
 			// TODO:: handle edge cases for words such as 'one-to-one,' 'let's,' 'dec. (??)'
 			while((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) { // ascii range for 'a-z' and 'A-Z'
 				
-				(char_frequencies[c])++;
+				if (simple_sentence) {
+					(char_frequencies_simple[c])++;
+				} else {
+					(char_frequencies_complex[c])++;
+				}
+				
 				word_length++;
 				i++;
 				if (i < input_buffer.length()) {
@@ -50,50 +55,65 @@ int main() {
 				}
 				
 				// check for words in the form 'one-to-one' etc.
-				// N.B.: hyphens are counted in the char frequency if they constitute a word
 				if (c == '-') {
 					if (i + 1 < input_buffer.length() && 
-						(input_buffer.at(i + 1) >= 65 && input_buffer.at(i + 1) <= 90) || 
-						(input_buffer.at(i + 1) >= 97 && input_buffer.at(i + 1) <= 122)) {
+						((input_buffer.at(i + 1) >= 65 && input_buffer.at(i + 1) <= 90) || 
+						(input_buffer.at(i + 1) >= 97 && input_buffer.at(i + 1) <= 122))) {
 
-						(char_frequencies[c])++; // it's a word --> increment hyphen count
+						if (simple_sentence) { // it's a word --> increment hyphen count
+							(char_frequencies_simple[c])++;
+						} else {
+							(char_frequencies_complex[c])++;
+						} 
+
 						word_length++;
 						c = input_buffer.at(i + 1);
-
 					}
 				}
 
 				// N.B.: words such as 'don't' are defined to be length 4
-				if (c == 39) { // TODO:: other types of potential apostrophes
+				if (c == 39) { 
 					if (i + 1 < input_buffer.length() &&
-                                                (input_buffer.at(i + 1) >= 65 && input_buffer.at(i + 1) <= 90) ||
-                                                (input_buffer.at(i + 1) >= 97 && input_buffer.at(i + 1) <= 122)) {
+                        ((input_buffer.at(i + 1) >= 65 && input_buffer.at(i + 1) <= 90) ||
+                        (input_buffer.at(i + 1) >= 97 && input_buffer.at(i + 1) <= 122))) {
                                         
-                                                c = input_buffer.at(i + 1);
+						if (simple_sentence) { // it's a word --> increment apostrophe count
+							(char_frequencies_simple[c])++;
+						} else {
+							(char_frequencies_complex[c])++;
+						} 
 
-                                        } 
-
+                        c = input_buffer.at(i + 1);
+                    } 
 				}
 
-			if (word_length != 0) {
-				(length_frequencies[word_length])++;
+				if (word_length != 0) {
+				(length_frequencies_complex[word_length])++;
+				}
 			}
-			}
-
 		}
 
+		if (simple_sentence == false) { // just processed a complex sentence --> indicates we are now simple
+			simple_sentence = true;
+		}
+
+		if (input_buffer == "") {
+			simple_sentence = false; // complex sentence for given data file begin after a newline has been reached
+		}
 	}
 
-	print_data(char_frequencies, length_frequencies);
+	printf("----------------COMPLEX DATA-----------------\n");
+	print_data(char_frequencies_complex, length_frequencies_complex);
+	printf("----------------SIMPLE DATA------------------\n");
+	print_data(char_frequencies_simple, length_frequencies_simple);
 	return 0;
 }
 
-void print_data(map<char, unsigned> char_frequencies, map<unsigned, unsigned> length_frequencies) {
-	for (map<char, unsigned>::iterator it = char_frequencies.begin(); it != char_frequencies.end(); it++) {
+void print_data(map<char, unsigned> char_frequencies_complex, map<unsigned, unsigned> length_frequencies_complex) {
+	for (map<char, unsigned>::iterator it = char_frequencies_complex.begin(); it != char_frequencies_complex.end(); it++) {
 		std::cout << it->first << " " << it->second << std::endl;
 	}
-	for (map<unsigned, unsigned>::iterator it = length_frequencies.begin(); it != length_frequencies.end(); it++) {
+	for (map<unsigned, unsigned>::iterator it = length_frequencies_complex.begin(); it != length_frequencies_complex.end(); it++) {
 		std::cout << it->first << " " << it->second << std::endl;
 	}
-
 }
