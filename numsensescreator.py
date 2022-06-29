dab060ed92569ec37b6e1a7907a6cc40c253a87e
sertman1 <sertman1@jhu.edu>
2022-06-29 13:10:48 +0200

    codebase organization

--- numsensescreator.py
+++ numsensescreator.py
@@ -1,8 +1,8 @@
 import csv
 
 API = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-trial_candidate_file = '/home/hulatpc/Downloads/candidates.csv'
-test_candidate_file = '/home/hulatpc/Downloads/candidatestest.csv'
+trial_candidate_file = './assets/candidates.csv'
+test_candidate_file = './assets/candidatestest.csv'
 
 def main():
     tsvin = open(trial_candidate_file, "rt", encoding='utf-8')   
@@ -11,12 +11,22 @@ def main():
 
     tsvin2 = open(test_candidate_file, "rt", encoding='utf-8')   
     tsvin2 = csv.reader(tsvin2, delimiter='\t')
-    trial_candidate_list = list()
+    test_candidate_list = list()
+
+    for row in tsvin:
+        trial_candidate_list.append(row)
+
+    for row in tsvin2:
+        test_candidate_list.append(row)
 
     for i in range(len(trial_candidate_list)):
-        get_num_senses(trial_candidate_list[0])
+        get_num_senses(trial_candidate_list[i])
+
+    for i in range(len(test_candidate_list)):
+        get_num_senses(test_candidate_list[i])
 
 def get_num_senses(candidates):
+    
     return
 
 if __name__ == '__main__':
