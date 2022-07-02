f9640073eb5ddef2f494224e8dd28a7ea47e087d
sertman1 <sertman1@jhu.edu>
2022-07-02 11:09:52 +0200

    num senses creator complete

--- numsensescreator.py
+++ numsensescreator.py
@@ -1,7 +1,6 @@
 import csv
-from operator import contains
-
-from attr import has
+import requests
+import time
 
 API = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 trial_candidate_file = './assets/candidates.csv'
@@ -11,14 +10,14 @@ num_senses_file = './assets/numberofsenses.csv'
 
 trial_candidate_list = list()
 test_candidate_list = list()
-num_senses_list = {"" : 0}
+num_senses_list = {}
 
 def retrieve_data_from_files():
     tsvin = open(trial_candidate_file, "rt", encoding='utf-8')   
     tsvin = csv.reader(tsvin, delimiter='\t')
     tsvin2 = open(test_candidate_file, "rt", encoding='utf-8')   
     tsvin2 = csv.reader(tsvin2, delimiter='\t')
-    tsvin3 = open(test_candidate_file, "rt", encoding='utf-8')   
+    tsvin3 = open(num_senses_file, "rt", encoding='utf-8')   
     tsvin3 = csv.reader(tsvin3, delimiter=',')
     
     for row in tsvin:
@@ -41,16 +40,34 @@ def tokenizecandidates(candidates):
     return listofcandidates
 
 def get_num_senses(candidate):
-    if num_senses_list in candidate: 
+    if candidate in num_senses_list:
         return # save time by not calling API unnecessarily
-    return
+    else:
+        numberofmeanings = 0 # a candidate not found in the API gets assigned 0c
+        dictionaryentry = requests.get(API + candidate)
+        if dictionaryentry.status_code == 200:
+            for i in range(len(dictionaryentry.json()[0]['meanings'])):
+                numberofmeanings += len(dictionaryentry.json()[0]['meanings'][i]['definitions'])
 
+        num_senses_list[candidate] = numberofmeanings # store retireved data to be written to csv
+        write_to_csv(num_senses_list)
+        time.sleep(0.33) # to prevent overwhelming the public API
+        
+def write_to_csv(dict):
+    # sort numberofsenses from highest to lowest
+    dict = {k: v for k, v in sorted(dict.items(), key=lambda item: item[1], reverse=True)}
+    print('writing to csv')
+
+    with open('./assets/numberofsenses.csv', 'w') as file:
+        for key in dict.keys():
+            file.write("%s, %s\n" % (key, dict[key]))
+    
 def main():
     retrieve_data_from_files()
-
     for i in range(len(trial_candidate_list)):
         for candidate in tokenizecandidates(trial_candidate_list[i]):
             get_num_senses(candidate)
+            
 
     for i in range(len(test_candidate_list)):
         for candidate in tokenizecandidates(test_candidate_list[i]):
